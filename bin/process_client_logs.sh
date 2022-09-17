ad025a34c6366338ef3b23baac7151d89e18c0fc
Dongting Cai <cai00151@morris.umn.edu>
2022-09-17 18:36:10 -0500

    Update client log process, code impv, not pass
    < Issue #1 >
    Update process_client_logs.sh, with code improvement that uses Perl as a filter. We are still having some issues and figuring out what happened.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #Step 1: Move to the specified directory.
-cd "$1" || exit
-tarDir=$(pwd)
+cd "$1" || return
+#tarDir=$(pwd)
 
 #Step 2: Gather the contents of all the log files in that directory and pipe them to a command that…
-cat $tarDir/log/secure* > merge_logs
+#$tarDir/log/secure* > merge_logs
+cat var/log/secure* | perl -pe 's/(.*?(?:\d(?=:))).*Failed.* ([a-z]+) from ([\d.]+).*/\1 \2 \3\n/p' > failed_login_data.txt
 
 #Step 3: Extracts the appropriate columns from the relevant lines, piping them to a command that…
-tempText=$(sed -n -E 's/(.*sshd.* Failed\spassword .*)/\1/p' < merge_logs)
+#tempText=$(sed -n -E 's/(.*sshd.* Failed\spassword .*)/\1/p' < merge_logs)
 
 #Step 4: Removes the minutes and seconds from all the times, then…
 #Step 5: Redirect the output to the file failed_login_data.txt
-sed -n -E 's/(.*?(?:\d(?=:))).* ([a-z]+) from ([\d.]+).*/\1 \2 \3\n/p' < "$tempText" > failed_login_data.txt
+#sed -n -E 's/(.*?(?:\d(?=:))).* ([a-z]+) from ([\d.]+).*/\1 \2 \3\n/p' < "$tempText" > failed_login_data.txt
