3cadabd9322e978a9274ad0d5073b92ebd88beda
Dongting Cai <cai00151@morris.umn.edu>
2022-09-17 21:17:10 -0500

    Clean process_client_logs.sh
    < Issue #1 >
    Clean process_client_logs.sh, finish Issue.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,17 +2,9 @@
 
 #Step 1: Move to the specified directory.
 cd "$1" || exit
-#tarDir=$(pwd)
 
-#Step 2: Gather the contents of all the log files in that directory and pipe them to a command that…
-#cat var/log/secure* > merge_logs
-#cat var/log/secure* | perl -ne 'print if s/(.*?(?:\d(?=:))).*Failed.* ([a-z]+) from ([\d.]+).*/\1 \2 \3/p' > failed_login_data.txt
-
-cat var/log/secure* | perl -ne 'print if s/([A-Za-z]+)\W*(\d+ \d+).*Failed.* (\S+) from ([\d.]+).*/\1 \2 \3 \4/p' > failed_login_data.txt
-
-#Step 3: Extracts the appropriate columns from the relevant lines, piping them to a command that…
-#tempText=$(sed -n -E 's/(.*sshd.* Failed\spassword .*)/\1/p' < merge_logs)
-
-#Step 4: Removes the minutes and seconds from all the times, then…
-#Step 5: Redirect the output to the file failed_login_data.txt
-#sed -n -E 's/(.*?(?:\d(?=:))).* ([a-z]+) from ([\d.]+).*/\1 \2 \3\n/p' < "$tempText" > failed_login_data.txt
+#Step 2: Gather the contents of all the log files in that directory and pipe them
+#Step 3: Extracts the appropriate columns from the relevant lines
+#Step 4: Removes the minutes and seconds from all the times
+#Step 5: Redirect the output to the file
+cat var/log/secure* | perl -ne 'print if s/([A-Za-z]+)\W*(\d+ \d+).*Failed.* (\S+) from ([\d.]+).*/\1 \2 \3 \4/' > failed_login_data.txt
