25d5923404a5f238cb1f64d3f5ee239f91f2eb68
Dongting Cai <cai00151@morris.umn.edu>
2022-09-17 21:14:00 -0500

    Update process_client_logs.sh, code impv, test pass
    < Issue #1 >
    Update process_client_logs.sh, with code improvement, which passes the test and shellcheck. This commit contains uncleaned code for future reference, and will delete unused code in the next commit.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #Step 1: Move to the specified directory.
-cd "$1" || return
+cd "$1" || exit
 #tarDir=$(pwd)
 
 #Step 2: Gather the contents of all the log files in that directory and pipe them to a command that…
-#$tarDir/log/secure* > merge_logs
-cat var/log/secure* | perl -pe 's/(.*?(?:\d(?=:))).*Failed.* ([a-z]+) from ([\d.]+).*/\1 \2 \3\n/p' > failed_login_data.txt
+#cat var/log/secure* > merge_logs
+#cat var/log/secure* | perl -ne 'print if s/(.*?(?:\d(?=:))).*Failed.* ([a-z]+) from ([\d.]+).*/\1 \2 \3/p' > failed_login_data.txt
+
+cat var/log/secure* | perl -ne 'print if s/([A-Za-z]+)\W*(\d+ \d+).*Failed.* (\S+) from ([\d.]+).*/\1 \2 \3 \4/p' > failed_login_data.txt
 
 #Step 3: Extracts the appropriate columns from the relevant lines, piping them to a command that…
 #tempText=$(sed -n -E 's/(.*sshd.* Failed\spassword .*)/\1/p' < merge_logs)
