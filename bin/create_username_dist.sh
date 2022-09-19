1befc3d0e8f7700ada7dffe5dbfd2dc24c7172c4
Dongting Cai <cai00151@morris.umn.edu>
2022-09-19 16:29:05 -0500

    Finish create_username_dist.sh, test pass
    < Issue #2 >
    Finish create_username_dist.sh, which passes both the bats test and shellcheck.

---
+++ bin/create_username_dist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Step 1: Filter the data in failed_login_data.txt 
+# by only getting the usernames, put it into a temporary file
+
+tempFile=$(mktemp)
+cat "$1"/*/failed_login_data.txt | perl -ne 'print if s/.*\s\d+\s(\S+).*/\1/' > "$tempFile"
+
+#Step 2: Sort tempFile, count username and output 
+#username_dist.html with correct format
+
+sort "$tempFile" | uniq -c | perl -ne 'print if s/(\d+)\s(\S+)/data.addRow([\x27\2\x27, \1]);' | bin/wrap_contents.sh /dev/stdin html_components/username_dist "$1"/username_dist.html
+ 
