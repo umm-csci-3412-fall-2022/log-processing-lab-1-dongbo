a9f0b1f3ef710555f454fb61ac2a117c54930194
Dongting Cai <cai00151@morris.umn.edu>
2022-09-19 16:56:10 -0500

    Fix create_username_dist.sh, pass test
    < Issue #2 >
    Fix create_username_dist.sh to correct files that can pass the test successfully. For the previous commit, due to an accidental local file change that passes the bats test, which should not pass at that time.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,10 +4,10 @@
 # by only getting the usernames, put it into a temporary file
 
 tempFile=$(mktemp)
-cat "$1"/*/failed_login_data.txt | perl -ne 'print if s/.*\s\d+\s(\S+).*/\1/' > "$tempFile"
+cat "$1"/*/failed_login_data.txt | perl -ne 'print if s/.* \d+ (\S+).*/\1/' > "$tempFile"
 
 #Step 2: Sort tempFile, count username and output 
 #username_dist.html with correct format
 
-sort "$tempFile" | uniq -c | perl -ne 'print if s/(\d+)\s(\S+)/data.addRow([\x27\2\x27, \1]);' | bin/wrap_contents.sh /dev/stdin html_components/username_dist "$1"/username_dist.html
+sort "$tempFile" | uniq -c | perl -ne 'print if s/(\d+) (\S+)/data.addRow([\x27\2\x27, \1])\x3B/' | bin/wrap_contents.sh /dev/stdin html_components/username_dist "$1"/username_dist.html
  
