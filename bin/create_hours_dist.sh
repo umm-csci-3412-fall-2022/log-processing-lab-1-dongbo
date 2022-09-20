3d80978517947a78e3409e178423c75802e0db47
Dongting Cai <cai00151@morris.umn.edu>
2022-09-19 19:42:23 -0500

    Finish create_hours_dist.sh, test pass
    < Issue #3 >
    Finish create_hours_dist.sh, which passes both the bats test and shellcheck.

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Step 1: Filter the data in failed_login_data.txt
+# by only getting the hours, put it into a temporary file
+
+tempFile=$(mktemp)
+cat "$1"/*/failed_login_data.txt | perl -ne 'print if s/.* (\d+) \S+.*/\1/' > "$tempFile"
+
+#Step 2: Sort tempFile, count hours and output
+#hours_dist.html with correct format
+
+sort "$tempFile" | uniq -c | perl -ne 'print if s/(\d+) (\d+)/data.addRow([\x27\2\x27, \1])\x3B/' | bin/wrap_contents.sh /dev/stdin html_components/hours_dist "$1"/hours_dist.html
